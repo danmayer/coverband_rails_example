2119a8f954fe62c2f754e4c078d186d1de6724b9
Dan Mayer <danmayer@gmail.com>
2024-02-17 19:22:31 -0700

    add coverband and basic configuration

---
+++ config/coverband.rb
@@ -0,0 +1,18 @@
+Coverband.configure do |config|
+    config.logger = Rails.logger
+  
+    # config options false, true. (defaults to false)
+    # true and debug can give helpful and interesting code usage information
+    # and is safe to use if one is investigating issues in production, but it will slightly
+    # hit perf.
+    config.verbose = false
+  
+    # default false. button at the top of the web interface which clears all data
+    config.web_enable_clear = true
+
+    # avoid all your servers reporting at once... In production I recommend 30
+    config.reporting_wiggle = 2
+
+    # in developer mode we want to report more often than in production, in production I often set this to every 5 minutes (300 seconds)
+    config.background_reporting_sleep_seconds = 2
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+
+  # This is how you mount coverband
+  # NOTE: This is a demo, in a real app you would want to secure this route
+  mount Coverband::Reporters::Web.new, at: '/coverage'
+
+  # For example, to protect the route
+  # authenticate :user, lambda { |u| u.admin? } do
+  #   mount Coverband::Reporters::Web.new, at: '/coverage'
+  # end
 end
